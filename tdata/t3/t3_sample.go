c6d4cda48b4898180df9a54553227d37ec38c7c4
Shinya Hayashi <shinyaUT0925@gmail.com>
2024-11-23 20:17:19 +0900

    add test patterns
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- tdata/t3/t3_sample.go
+++ tdata/t3/t3_sample.go
@@ -1,5 +1,7 @@
 package t3
 
+import "github.com/peng225/silkroad/tdata/t1/t11"
+
 type AliasForST100 ST100
 type AliasForMapST100 map[string]ST100
 type AliasForSliceST100 []ST100
@@ -9,6 +11,7 @@ type AliasForChanInt chan AliasForInt
 type AliasForInt int
 type AliasForAny any
 type AliasForEmptyStruct struct{}
+type AliasForFunc func(*t11.ST1) *t11.ST2
 
 type ST100 struct {
 	a any
@@ -36,5 +39,21 @@ type ST103 struct {
 	i <-chan AliasForInt
 	j chan<- AliasForInt
 	k chan int
+}
+
+type ST104[T comparable] struct {
+	a complex128
+	b complex64
+}
+
+type ST105 struct {
+	error
+	f func(*t11.ST1) *t11.ST2
+	g ST101[t11.ST3]
+	h ST104[int]
+}
+
+type ST106 struct {
+	f AliasForFunc
 	l AliasForChanInt
 }
